6aa5be2032045e67da5be27b09dc1d4c8541fddf
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2023-02-16 20:11:21 +0000

    Initial Commit

---
+++ SwiftEnum/SwiftEnumApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftEnumApp.swift
+//  SwiftEnum
+//
+//  Created by Tony Chaidinis on 16/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftEnumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
