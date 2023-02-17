e3a30104b0d34002f19c7075788da6e391285b83
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2023-02-17 19:45:18 +0000

    enum class

--- SwiftEnum/DaysOfTheWeek.swift
+++ SwiftEnum/DaysOfTheWeek.swift
@@ -13,35 +13,42 @@ enum  Days {
   case friday
   case saturday
   case sunday
-  
-  func message {
-    switch Days {
-    case .monday:
-      return "Have a nice monday"
-      
-    default:
-      return nil:
-    }
+}
+
+func weekdayMessage(day: Days) -> String {
+  switch day {
+  case .monday:
+    return "Have a nice monday"
+  case .tuesday:
+    return "Have a nice tuesday"
+  case .wednesday:
+    return "Have a nice wednesday"
+  case .thursday:
+    return "Have a nice thursday"
+  case .friday:
+    return "Have a nice friday"
+  case .saturday:
+    return "Have a nice saturday"
+  case .sunday:
+    return "Have a nice sunday"
   }
 }
 
 struct DaysOfTheWeek: View {
     @State private var selectedFlavor: Days = .monday
     var body: some View {
-        Text("Give me a days of the week")
+      Text(weekdayMessage(day: selectedFlavor))
       List {
           Picker("Flavor", selection: $selectedFlavor) {
-              Text("monday").tag(Days.monday)
-              Text("tuesday").tag(Days.tuesday)
-              Text("wednesday").tag(Days.wednesday)
+            Text("monday").tag(Days.monday)
+            Text("tuesday").tag(Days.tuesday)
+            Text("wednesday").tag(Days.wednesday)
             Text("thursday").tag(Days.thursday)
             Text("friday").tag(Days.friday)
             Text("saturday").tag(Days.saturday)
             Text("sunday").tag(Days.sunday)
           }
       }
-      
-      Text(Days.message($selectedFlavor))
     }
 }
 
