8cb7796307f6693cb5f4b340bfb659925a54fdc9
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2023-02-17 21:39:55 +0000

    cc

---
+++ SwiftEnum/PlanetFromSun.swift
@@ -0,0 +1,60 @@
+//
+//  SwiftUIView.swift
+//  SwiftEnum
+//
+
+import SwiftUI
+
+enum Planets {
+  case earth
+  case mercury
+  case jupider
+  case venus
+  case saturn
+  case uranus
+  case neptune
+}
+
+func distanceFromSum(planet: Planets) -> String {
+  switch planet {
+    
+  case .earth:
+   return "150 km"
+  case .mercury:
+    return "69.728 million km"
+  case .jupider:
+    return "740.72 million km"
+  case .venus:
+    return "108.27 million km"
+  case .saturn:
+    return "1.4677 billion km"
+  case .uranus:
+    return "2.9405 billion km"
+  case .neptune:
+    return "4.4735 billion km"
+  }
+}
+
+struct PlanetFromSun: View {
+  @State private var planet: Planets = .earth
+    var body: some View {
+        Text("The distance from the sun")
+      Text(distanceFromSum(planet: planet))
+      List {
+        Picker("Planets", selection: $planet) {
+          Text("earth").tag(Planets.earth)
+          Text("mercury").tag(Planets.mercury)
+          Text("jupider").tag(Planets.jupider)
+          Text("venus").tag(Planets.venus)
+          Text("uranus").tag(Planets.uranus)
+          Text("neptune").tag(Planets.neptune)
+        }
+      }
+    }
+}
+
+struct PlanetFromSun_Previews: PreviewProvider {
+    static var previews: some View {
+        PlanetFromSun()
+    }
+}
---
+++ SwiftEnum/Temperature.swift
@@ -0,0 +1,48 @@
+//
+//  Temperature.swift
+//  SwiftEnum
+//
+
+import SwiftUI
+
+enum Temperatures {
+  case cold
+  case warm
+  case hot
+}
+
+func tempCalc(temp: Int) -> Temperatures {
+  
+  switch temp {
+  case -10...0:
+    return Temperatures.cold
+    
+  case 0..<10:
+    return Temperatures.warm
+    
+  case 10..<20:
+    return Temperatures.hot
+  default:
+    return Temperatures.hot
+  }
+  
+}
+
+struct Temperature: View {
+  @State var text = String()
+  @State var anotherText: Temperatures = .cold
+    var body: some View {
+      Text("Give me a temperature")
+      TextField("Give me an Int", text: $text)
+      Button("Submit") {
+        anotherText = tempCalc(temp: Int(text)!)
+        print(anotherText)
+      }
+    }
+}
+
+struct Temperature_Previews: PreviewProvider {
+    static var previews: some View {
+        Temperature()
+    }
+}
