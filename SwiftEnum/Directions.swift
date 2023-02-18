8a331f20711b4d3ebf11adb0027b06a40dc74212
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2023-02-18 21:15:10 +0000

    cc

--- SwiftEnum/Directions.swift
+++ SwiftEnum/Directions.swift
@@ -1,8 +1,22 @@
 
 import SwiftUI
 
-enum {
+enum Direction: String {
   case north, south, east, west
+}
+
+func oppositeDirection(from given: Direction ) -> String {
+  
+  switch given {
+  case .north:
+    return Direction.south.rawValue
+  case .south:
+    return Direction.north.rawValue
+  case .east:
+    return Direction.west.rawValue
+  case .west:
+    return Direction.east.rawValue
+  }
 }
 
 struct Directions: View {
