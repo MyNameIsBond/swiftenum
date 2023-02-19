e72c1f262bcd783791db3f4de32a8d1be6a1ed43
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2023-02-19 12:25:54 +0000

    cc

--- SwiftEnum/ContentView.swift
+++ SwiftEnum/ContentView.swift
@@ -10,7 +10,7 @@ struct ContentView: View {
 //      HertzNotes()
 //      StatusCodes()
 //        Directions()
-      Deck()
+       Deck()
     }
 }
 
---
+++ SwiftEnum/TicTacToe.swift
@@ -0,0 +1,16 @@
+
+import SwiftUI
+
+
+
+struct TicTacToe: View {
+    var body: some View {
+        Text("Tic Tac Toe Board")
+    }
+}
+
+struct TicTacToe_Previews: PreviewProvider {
+    static var previews: some View {
+        TicTacToe()
+    }
+}
