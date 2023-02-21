c3b717cb09a254c0bc49693f09f80fc0e8f143b2
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2023-02-21 21:43:15 +0000

    cc

--- SwiftEnum/ChessBoard.swift
+++ SwiftEnum/ChessBoard.swift
@@ -31,20 +31,22 @@ enum PlauersTurn {
   case white, black
 }
 
-struct PieceOnTheBoard {
+struct PieceOnTheBoard: Hashable {
   let Row: Squares
   let Column: Int
   let Piece: Pieces
 }
 
-func setUpBoard() -> [PieceOnTheBoard] {
-  var myBoard:[PieceOnTheBoard] = []
+func setUpBoard() -> [[PieceOnTheBoard]] {
+  var myBoard:[[PieceOnTheBoard]] = []
   
   for square in Squares.allCases {
+    var myRow:[PieceOnTheBoard] = []
     for inSqaure in 1...8 {
-      
-      myBoard.append(.init(Row: square, Column: inSqaure, Piece: setUpPiecesOnNewGame(row: square, column: inSqaure)))
+      myRow.append(.init(Row: square, Column: inSqaure, Piece: setUpPiecesOnNewGame(row: square, column: inSqaure)))
     }
+    myBoard.append(myRow)
+    myRow.removeAll()
   }
   print(myBoard)
   return myBoard
@@ -54,14 +56,14 @@ struct ChessBoard: View {
   @State var board = setUpBoard()
   var body: some View {
     HStack(spacing: 1) {
-      ForEach(1..<9) { r in
+      ForEach(board, id: \.self) { r in
         VStack(spacing: 1) {
-          ForEach(1..<Squares.allCases.count) { c in
+          ForEach(r, id: \.self) { c in
             ZStack {
               Rectangle()
-                .fill(((r+c)%2 == 0) ? .brown : .black)
+//                .fill(((r+c)%2 == 0) ? .brown : .black)
                 .frame(width: 80, height: 80)
-              Text(Pieces.queen.rawValue).foregroundColor(.white).font(.system(size: 50))
+              Text(c.Piece).foregroundColor(.white).font(.system(size: 50))
             }.onTapGesture {
               print(r,$0)
             }
