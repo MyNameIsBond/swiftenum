672c813bd63741a3d809c9c2fe87b8197472121f
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2023-02-21 17:47:00 +0000

    chess board created

--- SwiftEnum/ChessBoard.swift
+++ SwiftEnum/ChessBoard.swift
@@ -1,9 +1,42 @@
 
 import SwiftUI
 
+enum Pieces {
+  case king, queen, bishop, knight, rook, pawn
+}
+
+enum Squares: CaseIterable {
+  case a,b,c,d,e,f,g,h
+}
+
+enum PlauersTurn {
+  case white, black
+}
+
+enum Sqaure {
+  case Pieces
+  case empty
+}
+
+
 struct ChessBoard: View {
+  
   var body: some View {
-    Text("Chess Board")
+    HStack(spacing: 3) {
+      ForEach(0..<8) { r in
+        VStack(spacing: 3) {
+          ForEach(0..<8) { c in
+            ZStack{
+              Rectangle().fill(((r+c)%2 == 0) ? .brown : .black)
+                .frame(width: 80, height: 80)
+            }.onTapGesture {
+              print(r,c)
+            }
+          }
+          
+        }
+      }
+    }
   }
 }
 
