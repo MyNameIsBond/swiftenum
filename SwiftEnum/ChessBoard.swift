1ba97262c9afcd9a289f35e4a855c952aa322cde
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2023-02-21 19:04:31 +0000

    cc

--- SwiftEnum/ChessBoard.swift
+++ SwiftEnum/ChessBoard.swift
@@ -1,12 +1,32 @@
 
 import SwiftUI
 
-enum Pieces {
-  case king, queen, bishop, knight, rook, pawn
+enum WhitePieces: String {
+  case king = "♔"
+  case queen = "♕"
+  case bishop = "♗"
+  case knight = "♘"
+  case rook = "♖"
+  case pawn = "♙"
 }
 
-enum Squares: CaseIterable {
+enum BlackPieces: String {
+  case king = "♚"
+  case queen = "♛"
+  case bishop = "♝"
+  case knight = "♞"
+  case rook = "♜"
+  case pawn = "♟"
+}
+
+enum Squares: CaseIterable, Identifiable {
   case a,b,c,d,e,f,g,h
+  var id: Squares { self }
+}
+
+enum Selected {
+  case WhitePieces
+  case BlackPieces
 }
 
 enum PlauersTurn {
@@ -22,15 +42,17 @@ enum Sqaure {
 struct ChessBoard: View {
   
   var body: some View {
-    HStack(spacing: 3) {
-      ForEach(0..<8) { r in
-        VStack(spacing: 3) {
-          ForEach(0..<8) { c in
-            ZStack{
-              Rectangle().fill(((r+c)%2 == 0) ? .brown : .black)
+    HStack(spacing: 1) {
+      ForEach(1..<9) { r in
+        VStack(spacing: 1) {
+          ForEach(1..<Squares.allCases.count) { c in
+            ZStack {
+              Rectangle()
+                .fill(((r+c)%2 == 0) ? .brown : .black)
                 .frame(width: 80, height: 80)
+              Text(WhitePieces.queen.rawValue).foregroundColor(.white).font(.system(size: 50))
             }.onTapGesture {
-              print(r,c)
+              print(r,$0)
             }
           }
           
